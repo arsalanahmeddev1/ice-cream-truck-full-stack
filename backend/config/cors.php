f61df2d7828e873cc4a12a556b3f6e7102654956
Arsalan Ahmed <arsalanahmeddev1@gmail.com>
2026-04-21 05:13:12 +0500

    changes in cors

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -14,6 +14,7 @@ return [
         'http://127.0.0.1:3000',
         'http://localhost:8000',
         'http://127.0.0.1:8000',
+        'https://myicecreamtruck.sitestaginglink.com',
     ],
 
     'allowed_origins_patterns' => [],
